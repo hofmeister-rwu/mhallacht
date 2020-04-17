1f2480bad2840d6456ea7435bccf1f8c6f69ac7c
qlRitscher <ritscher@skouz.de>
2020-04-17 16:42:31 +0200

    Connect to database

--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -79,176 +79,189 @@ class Card {
 
 
 
-//Draw the first Card of the Stack
-function draw(){
-    CardStore.selectStackCard(this.gameBoard.cardsInMiddle[0]);
-}
+    //Draw the first Card of the Stack
+    function draw(){
+        CardStore.selectStackCard(this.gameBoard.cardsInMiddle[0]);
+    }
 
-//Throw a Card that the Player doesnt want on the used Cards Stack
-function throwCards(){
-  //if you dont want to use the StackCard throw it on the usedCards Stack
-    let thrownCard = this.gameBoard.cardsInMiddle.splice(0,1);
-    this.gameBoard.usedCards.splice(0,0,thrownCard[0]);
-    this.endTurn();
-}
+    //Throw a Card that the Player doesnt want on the used Cards Stack
+    function throwCards(){
+      //if you dont want to use the StackCard throw it on the usedCards Stack
+        let thrownCard = this.gameBoard.cardsInMiddle.splice(0,1);
+        this.gameBoard.usedCards.splice(0,0,thrownCard[0]);
+        this.endTurn();
+    }
 
-//Swap a Card from the current player with the first Card from the Stack
-function swapStackCard(chosenCard){
-    //if a Card has been chosen, swap the first Card in the Stack with the chosenCard
-    if(chosenCard!=undefined){
+    //Swap a Card from the current player with the first Card from the Stack
+    function swapStackCard(chosenCard){
+        //if a Card has been chosen, swap the first Card in the Stack with the chosenCard
+        if(chosenCard!=undefined){
+            var chosenCardIndex;
+              for(let i = 0; i < this.gameBoard.players[this.state.activePlayerIndex].playerCards.length; i++){
+                if(this.gameBoard.players[this.state.activePlayerIndex].playerCards[i]==chosenCard){
+                  chosenCardIndex=i;
+                }
+              }
+            let playerCard = this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,1);
+            let stackCard = this.gameBoard.cardsInMiddle.splice(0,1);
+            this.gameBoard.usedCards.splice(0,0,playerCard[0]);
+            this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,0,stackCard[0]);
+            this.endTurn();
+
+        //if the chosenCard hasn't been defined yet, give out a warning
+        }else{
+          this.setState({alert:"Choose One of Your Cards to be Swapped by Clicking on it", warningshow:true})
+        }
+    }
+
+
+    //Swap a Card from the active Player with a Card from another player
+    function swapPlayerCard(chosenCard,enemyCard){
+      //if chosenCard and enemyCard are both defined
+      if(chosenCard!=undefined&&enemyCard!=undefined){
+
+        //getCardIndex of ChosenCard
         var chosenCardIndex;
           for(let i = 0; i < this.gameBoard.players[this.state.activePlayerIndex].playerCards.length; i++){
             if(this.gameBoard.players[this.state.activePlayerIndex].playerCards[i]==chosenCard){
               chosenCardIndex=i;
             }
           }
-        let playerCard = this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,1);
-        let stackCard = this.gameBoard.cardsInMiddle.splice(0,1);
-        this.gameBoard.usedCards.splice(0,0,playerCard[0]);
-        this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,0,stackCard[0]);
-        this.endTurn();
 
-    //if the chosenCard hasn't been defined yet, give out a warning
-    }else{
-      this.setState({alert:"Choose One of Your Cards to be Swapped by Clicking on it", warningshow:true})
-    }
-}
-
-
-//Swap a Card from the active Player with a Card from another player
-function swapPlayerCard(chosenCard,enemyCard){
-  //if chosenCard and enemyCard are both defined
-  if(chosenCard!=undefined&&enemyCard!=undefined){
-
-    //getCardIndex of ChosenCard
-    var chosenCardIndex;
-      for(let i = 0; i < this.gameBoard.players[this.state.activePlayerIndex].playerCards.length; i++){
-        if(this.gameBoard.players[this.state.activePlayerIndex].playerCards[i]==chosenCard){
-          chosenCardIndex=i;
-        }
-      }
-
-    //getCardIndex and PlayerIndex of enemyCard
-    var enemyCardIndex;
-    var enemyPlayerIndex;
-    for(let j=0; j < this.gameBoard.players.length; j++){
-      for(let i = 0; i < this.gameBoard.players[j].playerCards.length; i++){
-        if(this.gameBoard.players[j].playerCards[i]==enemyCard){
-          enemyCardIndex=i;
-          enemyPlayerIndex=j;
-        }
-      }
-    }
-    //Delete chosenCard and enemyCard out of their normal places and store them in an array
-    let playerCard = this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,1);
-    let otherCard = this.gameBoard.players[enemyPlayerIndex].playerCards.splice(enemyCardIndex,1);
-
-    //Insert chosenCard at place of enemyCard and Insert enemyCard inplace of chosenCard
-    this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,0,otherCard[0]);
-    this.gameBoard.players[enemyPlayerIndex].playerCards.splice(enemyCardIndex,0,playerCard[0]);
-
-
-    // put the StackCard on the UsedCard Staple and EndTUrn
-    let thrownCard = this.gameBoard.cardsInMiddle.splice(0,1);
-    this.gameBoard.usedCards.splice(0,0,thrownCard[0]);
-    this.endTurn();
-
-  //if one Card isn't chosen yet give out a warning
-}else{
-    if(chosenCard==undefined && enemyCard==undefined){
-        this.setState({alert:"Choose One of Your Cards to be Swapped by Clicking on it. Choose One of Your Enemies' Cards to be Swapped by Clicking on it", warningshow:true})
-    }
-    if(chosenCard==undefined && enemyCard!=undefined){
-        this.setState({alert:"Choose One of Your Cards to be Swapped by Clicking on it", warningshow:true})
-    }
-    if (enemyCard==undefined && chosenCard!=undefined){
-          this.setState({alert:"Choose One of Your Enemies' Cards to be Swapped by Clicking on it", warningshow:true})
-    }
-  }
-}
-
-//Show one of the activePlayers Cards
-function show(card){
-  if(card!=undefined){
-
-      //if the Card to Show is defined, show the Card Value for 2 seconds
-      CardStore.selectShowCard(card);
-      setTimeout(() => {this.endTurn();}, 2000);
-
-      //after the Timer has run out, put the StackCard on the UsedCard Staple
-      let thrownCard = this.gameBoard.cardsInMiddle.splice(0,1);
-      this.gameBoard.usedCards.splice(0,0,thrownCard[0]);
-
-  //if no card is chosen give out a warning
-  }else{
-    this.setState({alert:"Choose A Card to Show, by Clicking on one of your Cards!", warningshow:true})
-  }
-}
-
-//After the activePlayer has performed an action his Turn is over
-function endTurn(){
-  //if the EndGame has been defined, we check if the Game is over yet
-    if(this.state.endRound!=""){
-      this.checkEndGame();
-    }
-  //if theres a next player in the array it's his turn, so we unselect all Cards and count up the activePlayerIndex
-    if(this.state.activePlayerIndex+1<this.gameBoard.players.length){
-      this.setState({activePlayerIndex:this.state.activePlayerIndex+1,});
-      CardStore.unselectCards();
-    }else{
-  //if theres no next player in the array a new Round begins and we start with the first player again
-      this.endRound();
-    }
-}
-
-//if all Players had their turn a new round beings
-function endRound(){
-    this.setState({round:this.state.round+1,});
-    this.setState({activePlayerIndex:0});
-    CardStore.unselectCards();
-}
-
-//if a player presses the EndGame Button, the point in time when the Game will end (one Round after the player pressed) is defined
-function setEndGame(){
-  if(this.state.endRound=="" && this.state.endPlayer==""){
-    if(this.state.activePlayerIndex-1>=0){
-      this.setState({endPlayer:this.state.activePlayerIndex-1});
-      this.setState({endRound:this.state.round+1});
-    }else{
-      this.setState({endRound:this.state.round});
-      this.setState({endPlayer:this.gameBoard.players.length-1});
-    }
-  }
-}
-
-//if the defined point when the game ends, has come, the results are posted
-function checkEndGame(){
-  if(this.state.round == this.state.endRound && this.state.activePlayerIndex == this.state.endPlayer){
-    for(var i=0; i<this.gameBoard.players.length; i++){
-        this.gameBoard.players[i].playerValue=0;
-          for (var j = 0; j < this.gameBoard.players[i].playerCards.length; j++) {
-            if(this.gameBoard.players[i].playerCards[j].value!="show" && this.gameBoard.players[i].playerCards[j].value!="swap"){
-                this.gameBoard.players[i].playerValue += this.gameBoard.players[i].playerCards[j].value;
-            }else{
-              this.gameBoard.players[i].playerValue += 15;
+        //getCardIndex and PlayerIndex of enemyCard
+        var enemyCardIndex;
+        var enemyPlayerIndex;
+        for(let j=0; j < this.gameBoard.players.length; j++){
+          for(let i = 0; i < this.gameBoard.players[j].playerCards.length; i++){
+            if(this.gameBoard.players[j].playerCards[i]==enemyCard){
+              enemyCardIndex=i;
+              enemyPlayerIndex=j;
             }
           }
-    }
-    var endPlayers=[...this.gameBoard.players];
-    endPlayers.sort((a, b) => a.playerValue - b.playerValue);
-    let counter = 0;
-    const endList = endPlayers.map((player, key) =>{
-        counter++;
-        return(<div key={player.playerName}><Alert>#{counter}: {player.playerName} with {player.playerValue} </Alert></div>);
-    });
-    this.setState({alert:endList, warningshow:true, end:true})
-  }
-}
+        }
+        //Delete chosenCard and enemyCard out of their normal places and store them in an array
+        let playerCard = this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,1);
+        let otherCard = this.gameBoard.players[enemyPlayerIndex].playerCards.splice(enemyCardIndex,1);
 
-//ModalFunction
-function handleModalClose(){
-    this.setState({warningshow:false});
-}
+        //Insert chosenCard at place of enemyCard and Insert enemyCard inplace of chosenCard
+        this.gameBoard.players[this.state.activePlayerIndex].playerCards.splice(chosenCardIndex,0,otherCard[0]);
+        this.gameBoard.players[enemyPlayerIndex].playerCards.splice(enemyCardIndex,0,playerCard[0]);
+
+
+        // put the StackCard on the UsedCard Staple and EndTUrn
+        let thrownCard = this.gameBoard.cardsInMiddle.splice(0,1);
+        this.gameBoard.usedCards.splice(0,0,thrownCard[0]);
+        this.endTurn();
+
+      //if one Card isn't chosen yet give out a warning
+    }else{
+        if(chosenCard==undefined && enemyCard==undefined){
+            this.setState({alert:"Choose One of Your Cards to be Swapped by Clicking on it. Choose One of Your Enemies' Cards to be Swapped by Clicking on it", warningshow:true})
+        }
+        if(chosenCard==undefined && enemyCard!=undefined){
+            this.setState({alert:"Choose One of Your Cards to be Swapped by Clicking on it", warningshow:true})
+        }
+        if (enemyCard==undefined && chosenCard!=undefined){
+              this.setState({alert:"Choose One of Your Enemies' Cards to be Swapped by Clicking on it", warningshow:true})
+        }
+      }
+    }
+
+    //Show one of the activePlayers Cards
+    function show(card){
+      if(card!=undefined){
+
+          //if the Card to Show is defined, show the Card Value for 2 seconds
+          CardStore.selectShowCard(card);
+          setTimeout(() => {this.endTurn();}, 2000);
+
+          //after the Timer has run out, put the StackCard on the UsedCard Staple
+          let thrownCard = this.gameBoard.cardsInMiddle.splice(0,1);
+          this.gameBoard.usedCards.splice(0,0,thrownCard[0]);
+
+      //if no card is chosen give out a warning
+      }else{
+        this.setState({alert:"Choose A Card to Show, by Clicking on one of your Cards!", warningshow:true})
+      }
+    }
+
+    //After the activePlayer has performed an action his Turn is over
+    function endTurn(){
+      //if the EndGame has been defined, we check if the Game is over yet
+        if(this.state.endRound!=""){
+          this.checkEndGame();
+        }
+      //if theres a next player in the array it's his turn, so we unselect all Cards and count up the activePlayerIndex
+        if(this.state.activePlayerIndex+1<this.gameBoard.players.length){
+          this.setState({activePlayerIndex:this.state.activePlayerIndex+1,});
+          CardStore.unselectCards();
+        }else{
+      //if theres no next player in the array a new Round begins and we start with the first player again
+          this.endRound();
+        }
+    }
+
+    //if all Players had their turn a new round beings
+    function endRound(){
+        this.setState({round:this.state.round+1,});
+        this.setState({activePlayerIndex:0});
+        CardStore.unselectCards();
+    }
+
+    //if a player presses the EndGame Button, the point in time when the Game will end (one Round after the player pressed) is defined
+    function setEndGame(){
+      if(this.state.endRound=="" && this.state.endPlayer==""){
+        if(this.state.activePlayerIndex-1>=0){
+          this.setState({endPlayer:this.state.activePlayerIndex-1});
+          this.setState({endRound:this.state.round+1});
+        }else{
+          this.setState({endRound:this.state.round});
+          this.setState({endPlayer:this.gameBoard.players.length-1});
+        }
+      }
+    }
+
+    //if the defined point when the game ends has come, the results are posted
+    function checkEndGame(){
+      if(this.state.round == this.state.endRound && this.state.activePlayerIndex == this.state.endPlayer){
+        for(var i=0; i<this.gameBoard.players.length; i++){
+            this.gameBoard.players[i].playerValue=0;
+              for (var j = 0; j < this.gameBoard.players[i].playerCards.length; j++) {
+                if(this.gameBoard.players[i].playerCards[j].value!="show" && this.gameBoard.players[i].playerCards[j].value!="swap"){
+                    this.gameBoard.players[i].playerValue += this.gameBoard.players[i].playerCards[j].value;
+                }else{
+                  this.gameBoard.players[i].playerValue += 15;
+                }
+              }
+        }
+        var endPlayers=[...this.gameBoard.players];
+        endPlayers.sort((a, b) => a.playerValue - b.playerValue);
+        let counter = 0;
+        const endList = endPlayers.map((player, key) =>{
+            counter++;
+            return(<div key={player.playerName}><Alert>#{counter}: {player.playerName} with {player.playerValue} </Alert></div>);
+        });
+        this.setState({alert:endList, warningshow:true, end:true})
+      }
+    }
+
+    function save(playersFromServer){
+      if(playersFromServer==undefined){
+        for(let i = 0; i < this.gameBoard.players.length; i++){
+            CardStore.addNewPlayer(this.gameBoard.players[i]);
+        }
+      }else{
+        for(let i = 0; i < this.gameBoard.players.length; i++){
+            CardStore.editPlayer(this.gameBoard.players[i]);
+        }
+      }
+      CardStore.fetchPlayers();
+    }
+
+    //ModalFunction
+    function handleModalClose(){
+        this.setState({warningshow:false});
+    }
 
 
 // durch die Annotation @observer
@@ -274,8 +287,9 @@ export default class Game extends React.Component {
   }
     render() {
      console.log(this.gameBoard);
-     console.log(this.state.endRound);
-     console.log(this.state.endPlayer);
+     const {playersFromServer} = CardStore;
+     console.log(playersFromServer);
+
      //Load PlayerCards into {items}
       const playerCards = this.gameBoard.players.map((item, key) =>{
           var cardClick;
@@ -362,6 +376,7 @@ export default class Game extends React.Component {
               {drawButton}
               {showButton}
               {endGameButton}
+              <Button onClick={save.bind(this,playersFromServer)}>Save</Button>
             </div>
         );
     }
--- src/js/stores/cardStore.js
+++ src/js/stores/cardStore.js
@@ -35,6 +35,137 @@ class CardStore {
         this.showCard=undefined;
         this.stackCard=undefined;
     };
+
+    @observable error = '';
+    @observable playersFromServer;
+    @action.bound fetchPlayers() {
+        return fetch('http://localhost:3000/players', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+        })
+            .then(response => {
+                if (response.status >= 200 && response.status < 300) {
+                    response.json().then(json => {
+                        this.playersFromServer = json.players;
+                        console.log(this.playersFromServer);
+                    });
+
+                } else {
+                    this.error = "Error on fetching";
+                }
+            })
+            .catch(
+                error => {
+                    this.error = "Error on fetching";
+                    throw error;
+                }
+            );
+    }
+
+
+    @action addNewPlayer(newPlayer) {
+        return fetch('http://localhost:3000/players/new', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify({
+                playerName : newPlayer.playerName,
+                playerCardOne : newPlayer.playerCards[0].value.toString(),
+                playerCardTwo : newPlayer.playerCards[1].value.toString(),
+                playerCardThree : newPlayer.playerCards[2].value.toString(),
+                playerCardFour : newPlayer.playerCards[3].value.toString()
+            })
+          }).then(response => {
+                if (response.status >= 200 && response.status < 300) {
+                    response.json().then(json => {
+                        console.log(json);
+                        this.fetchPlayers();
+                    });
+
+                } else {
+                    this.error = "Error on fetching";
+                }
+            })
+            .catch(
+                error => {
+                    this.error = "Error on fetching";
+                    throw error;
+                }
+            );
+    }
+
+
+
+    @action deletePlayer(playerName) {
+        return fetch('http://localhost:3000/players/delete/' + playerName, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+
+        })
+            .then(response => {
+                if (response.status >= 200 && response.status < 300) {
+                    response.json().then(json => {
+                        console.log("player deleted");
+                         this.fetchCats();
+                    });
+
+                } else {
+                    this.error = "Error on fetching";
+                }
+            })
+            .catch(
+                error => {
+                    this.error = "Error on fetching";
+                    throw error;
+                }
+            );
+    }
+
+    @action editPlayer(editPlayer) {
+        return fetch('http://localhost:3000/players/edit/' + editPlayer.playerName, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify({
+                playerName : editPlayer.playerName,
+                playerCardOne : editPlayer.playerCards[0].value,
+                playerCardTwo : editPlayer.playerCards[1].value,
+                playerCardThree : editPlayer.playerCards[2].value,
+                playerCardFour : editPlayer.playerCards[3].value
+            })
+        })
+            .then(response => {
+                if (response.status >= 200 && response.status < 300) {
+                    response.json().then(json => {
+                        console.log("Player updated");
+                         this.fetchPlayers();
+                    });
+
+                } else {
+                    this.error = "Error on fetching";
+                }
+            })
+            .catch(
+                error => {
+                    this.error = "Error on fetching";
+                    throw error;
+                }
+            );
+    }
 }
 const store = new CardStore();
 
